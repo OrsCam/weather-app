40098045b7dd8fad32f37ee1297f133843ae912a
Orzcaa <camilleorsoni@hotmail.fr>
2020-04-22 19:31:02 +0200

    Final version

--- src/Components/WeatherContainer.js
+++ src/Components/WeatherContainer.js
@@ -1,17 +1,54 @@
 import React, { useState } from 'react';
 import '../Styles/Weather.css';
+import WeatherInfo from './WeatherInfo';
 
 function WeatherContainer() {
+    const API_KEY = 'ee98f86c3bf33a8b667e1c4611a2d8d4';
     const [searchQuery, setSearchQuery] = useState('');
     const [weatherData, setWeatherData] = useState({
         temp: null,
         humidity: null,
         desc: null,
         city: null,
-    })
+    });
+
+    const [isValidZipCode, setIsValidZipCode] = useState(true);
 
     function updateSearchQuery(event) {
-        setSearchQuery(event.target.value);
+        let zipCode = event.target.value;
+        let isValid = validateZipCode(zipCode);
+        setSearchQuery(zipCode);
+
+        if (isValid || zipCode === '' || isValid.length === 5) {
+            setIsValidZipCode(true);
+        } else {
+            setIsValidZipCode(false);
+        }
+
+    }
+
+    function validateZipCode(zipCode) {
+        let regex = /[0-9]{5}/;
+        return regex.test(zipCode);
+    }
+
+    function getWeatherData() {
+        if (!isValidZipCode || searchQuery === '') {
+            setIsValidZipCode(false);
+            return;
+        }
+        fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${searchQuery},us&appid=${API_KEY}`)
+            .then(response => response.json())
+            .then(data => setWeatherData({
+                temp: convertToFarenheit(data.main.temp),
+                humidity: data.main.humidity,
+                desc: data.weather[0].main,
+                city: data.name
+            }));
+    }
+
+    function convertToFarenheit(temp) {
+        return ((temp - 273.15) * (9.0 / 5) + 32).toFixed(0);
     }
 
     return (
@@ -23,14 +60,16 @@ function WeatherContainer() {
                         placeholder="Zip Code"
                         className="search-input"
                         onChange={updateSearchQuery}
+                        maxLength='5'
                     />
-                    <button className="material-icons">search</button>
+                    <button onClick={getWeatherData} className="material-icons">search</button>
                 </div>
             </header>
+            <p className="error">{isValidZipCode ? '' : 'Invalid Zip Code'}</p>
             <section className="weather-info">
-                {weatherData.temp === null ? <p>No weather to display<i className="material-icons">wb_sunny</i></p> : ''}
+                {weatherData.temp === null ? (<p>No weather to display<i className="material-icons">wb_sunny</i></p>) : <WeatherInfo data={weatherData} />}
             </section>
-        </section >
+        </section>
     )
 
 }
